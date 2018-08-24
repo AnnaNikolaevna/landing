aea28776c6b04ef14a8ed40abe6f63f1af62e6b6
AnnaNikolaevna <iris.8965@yandex.ua>
2018-08-24 18:08:08 +0300

    eadded landing
    eadded a map, footer, block with email? what-else

--- js/scripts.js
+++ js/scripts.js
@@ -41,10 +41,12 @@ function to_top(){
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  var uluru = {lat: 40.4106, lng: -73.5631};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {
+				zoom: 12, 
+				center: uluru,});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
